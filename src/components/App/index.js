1cf85304f9c36a97175d48967eff76562034aa68
Unknown <alejandro.x.flores@gmail.com>
2019-05-23 10:55:40 -0500

    Add Auth

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,9 @@
+/* aws-amplify */
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import awsExports from '../../aws-exports';
 /* app */
 import App from './App';
-export default App;
+
+Amplify.configure(awsExports);
+export default withAuthenticator(App);
